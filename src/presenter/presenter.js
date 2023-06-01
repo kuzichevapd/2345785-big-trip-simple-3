585d0ee4378fbcc2719494ad3b81a22115f9f76b
Polina <kuzicheva.pd@edu.spbstu.ru>
2023-06-01 22:16:13 +0300

    module5-t1

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -2,17 +2,45 @@ import { render } from '../framework/render.js';
 import TripEventsList from '../view/events-list.js';
 import EventsSortingForm from '../view/events-sorting-form.js';
 import TripEvent from '../view/trips-event.js';
+import TripEventPresenter from './trip-event-presenter.js';
+import { changeItem } from '../util';
 class TripPresenter {
   init(container, tripModel) {
     this.container = container;
     this.tripModel = tripModel;
     this.tripEventsData = tripModel.tripEvents;
-    const tripEvents = this.tripEventsData.map((tripData) => new TripEvent(tripData));
-    this.tripListComponent = new TripEventsList(tripEvents);
+    this.tripEvents = this.tripEventsData.map((tripData) => new TripEvent(tripData));
+    this.tripListComponent = new TripEventsList(this.tripEvents);
     render(new EventsSortingForm(), this.container);
     render(this.tripListComponent, this.container);
+    if (this.tripEvents.length === 0){
+      this.#renderEmptyList();
+    }
   }
-}
 
+  #renderEmptyList = () => {
+    this.tripListComponent.updateMessage();
+  };
+
+  #renderEvent = (task) => {
+    const tripEventPresenter = new TripEventPresenter(this.tripListComponent, this.#handleEventChange, this.#handleModeChange);
+    tripEventPresenter.init(task);
+    this.tripEventPresenter.set(task.id, tripEventPresenter);
+  };
+
+  #clearEventList = () => {
+    this.tripEventPresenter.forEach((presenter) => presenter.destroy());
+    this.tripEventPresenter.clear();
+  };
+
+  #handleEventChange = (updatedEvent) => {
+    this.tripEvents = changeItem(this.tripEvents, updatedEvent);
+    this.tripEventPresenter.get(updatedEvent.id).init(updatedEvent);
+  };
+
+  #handleModeChange = () => {
+    this.tripEventPresenter.forEach((presenter) => presenter.resetView());
+  };
+}
 
 export default TripPresenter;
--- src/util.js
+++ src/util.js
@@ -63,3 +63,17 @@ export function capitalize(string) {
   }
   return string[0].toUpperCase() + string.substring(1).toLowerCase();
 }
+
+export const changeItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
